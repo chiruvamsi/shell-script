b29d20c8abbeaaf84c9bde4278ce3c97888af562
chiruvamsi <kasachiruvamsi@gmail.com>
2025-01-01 14:14:35 +0530

    Add array files

---
+++ 01-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Movies=("Pushpa" "Devara" "OG")
+
+echo "The first movie is : ${Movies[0]}"
+echo "The Second movie is : ${Movies[1]}"
+echo "The third movie is : ${Movies[2]}"
+
+echo "All movies are: ${Movies[@]}"
+
